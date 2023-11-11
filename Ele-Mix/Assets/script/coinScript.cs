624594c677bb4cafb5b99e9d6bf3991d8d698bbc
Discord615 <vanbeau1@gmail.com>
2023-11-11 20:42:41 +0800

    Adding Coin

---
+++ Ele-Mix/Assets/script/coinScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class coinScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+
+    }
+}
